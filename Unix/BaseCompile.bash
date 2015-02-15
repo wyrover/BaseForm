e7d88f0c159ccc7c5fc6a25ffa7ea64a4eccb3c6
JonWBedard <JonWBedard@gmail.com>
2015-02-15 01:52:22 -0800

    Executable Update
    Mac OSX compatibility (Finally!)

--- Unix/BaseCompile.bash
+++ Unix/BaseCompile.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-g++ -o BaseForm -I ../CryptoGateway -I ../glGraphics -I ../WifiRC -pthread BaseMain.cpp baseForm.cpp basePopUps.cpp inetForm.cpp propertiesForm.cpp USBGenerationForm.cpp -lGL -lglut -lGLU
+VAR_ARG="g++ -o BaseForm -I ../CryptoGateway -I ../glGraphics -I ../WifiRC BaseMain.cpp baseForm.cpp basePopUps.cpp inetForm.cpp propertiesForm.cpp USBGenerationForm.cpp"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    VAR_ARG="$VAR_ARG -pthread -framework OpenGL -framework GLUT"
+else
+    VAR_ARG = "$VAR_ARG -pthread -lGL -lglut -lGLU"
+fi
+eval $VAR_ARG
